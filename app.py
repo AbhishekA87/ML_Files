a4f2d5f7306601e98ad1af84dc89d1b75bb4601f
AbhishekA87 <88573167+AbhishekA87@users.noreply.github.com>
2025-09-25 09:49:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 
+import streamlit as st
+import joblib
+import pandas as pd
+
+st.title("📊 My ML Portfolio")
+
+# Load model
+model = joblib.load("models/model.pkl")
+
+# User input
+age = st.number_input("Age", 18, 70, 30)
+distance = st.slider("Trip Distance", 1, 500, 50)
+
+# Predict button
+if st.button("Predict"):
+    df = pd.DataFrame([[age, distance]], columns=["age", "distance"])
+    result = model.predict(df)[0]
+    st.success(f"Prediction: {result}")
